ed616e108a3c289ed4a29a35a7317f143f31525b
ibabdulaziz2197 <ibabdulaziz2197@gmail.com>
2025-11-17 15:52:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+# 11
+i = "abcdefgh"
+print(i)
+
+t = i[1:10:2]
+print(t)
+
+# 13
+a = "abracadabra"
+print(a)
+
+t = a[1:11:2]
+print(t)
+
+# 15
+a = "GameOverMisson"
+print(a)
+
+t = a[4:8]
+print(t)
+
+# 16
+a = "123456789"
+print(a)
+
+t = a[4:]
+t2 = t[::-1]
+print(t2)
+
+# 17
+a = "indexing"
+print(a)
+
+t = a[2:5]
+print(t)
+
+# 18
+s = "sclingisPowerful"
+print(s)
+
+t = s[3:]
+print(t)
+
+# 19
+a = "abdefghij"
+print(a)
+
+t = a[-5:-2]
+print(t)
+
+# 20
+a = "ABCDEFGIHJ"
+print(a)
+
+t = a[-2:-8:-1]
+print(t)
